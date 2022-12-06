18dcac848c3ba8af6e1b45f1e8ae4cf2ac9e6123
robr0b <robertuntdm@gmail.com>
2022-12-06 19:47:49 +0200

    resolved issue of incorrect response message

--- login.php
+++ login.php
@@ -12,10 +12,15 @@ $stmt = $conn->prepare("select * from users where email=:email");
 $stmt->bindValue(":email", $email);
 $result = $stmt->execute();
 $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-foreach ($data as $row) {
-    if (password_verify($password, $row["password_hash"])) {
-        $data = ["user_id" => $row["user_id"], "email" => $row["email"]];
-        $payload = [$data];
+
+if (sizeof($data) === 0) {
+    echo json_encode(["success" => false, "message" => "Email or password is incorrect"]);
+}
+else {
+    $user_data = $data[0];
+    if (password_verify($password, $data[0]["password_hash"])) {
+        $response_data = ["user_id" => $user_data["user_id"], "email" => $user_data["email"]];
+        $payload = [$response_data];
         $secret = "Firebase JWT secret";
         $token = JWT::encode($payload, $secret, "HS256");
         echo json_encode(["success" => true, "email" => $email, "token" => $token]);
@@ -24,4 +29,3 @@ foreach ($data as $row) {
         echo json_encode(["success" => false, "message" => "Email or password is incorrect"]);
     }
 }
-
