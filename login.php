d4738c915b2f984e8550a2d356279174ad45a8f4
robr0b <robertuntdm@gmail.com>
2022-12-06 17:15:08 +0200

    testing login with 1 user

--- login.php
+++ login.php
@@ -1,10 +1,20 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 require_once "connection.php";
-$conn = getConnection();
+$email = $_POST["email"] ?? "";
+$password = $_POST["password"] ?? "";
 
+$conn = getConnection();
+$stmt = $conn->prepare("select * from users where email=:email");
+$stmt->bindValue(":email", $email);
+$result = $stmt->execute();
+$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+foreach ($data as $row) {
+    if (password_verify($password, $row["password_hash"])) {
+        $response = array("success" => true);
+        echo json_encode($response);
+    }
+}
 //$stmt = $conn->prepare("select * from users where email=:email and password_hash=:password_hash");
-echo $_POST["data"];
-echo $_POST["email"];
-echo $_POST["password"];
+
 
