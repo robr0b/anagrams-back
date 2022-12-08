5c4408457621ad2e41e7fd0ef9db24743e6bc5f3
robr0b <robertuntdm@gmail.com>
2022-12-08 15:24:30 +0200

    testing

--- save-wordbase.php
+++ save-wordbase.php
@@ -1,18 +1,9 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-require_once "validate-wordbase.php";
-require_once "connection.php";
-require_once "jwt-token.php";
-require_once "vendor/autoload.php";
+//header("Access-Control-Allow-Origin: *");
+//require_once "validate-wordbase.php";
+//require_once "connection.php";
+//require_once "jwt-token.php";
+//require_once "vendor/autoload.php";
 
-if (file_exists(".env")) {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-}
-try {
-    var_dump(getenv("JWT_SECRET"));
-}
-catch (Exception $exception) {
-    echo "error";
-}
+echo getenv("JWT_SECRET");
 //var_dump(tokenIsValid($_POST["token"]));
